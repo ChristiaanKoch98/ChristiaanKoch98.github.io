5da328f5b13a842a4320c9a0ea6a94b0ad00410e
ChristiaanKoch98 <28427955@student.g.nwu.ac.za>
2020-08-19 14:18:46 +0200

    Code behind drag and drop area

--- JSONFileEditor.js
+++ JSONFileEditor.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
 
+var myWebsiteURL = "https://christiaankoch98.github.io/";
+
 //Load a selected JSON file
 function LoadFile (selectedFile) {
   //Convert from JSON object to JS object
@@ -22,3 +24,78 @@ function LoadFile (selectedFile) {
   return output;
 }
 
+
+
+//Drag and drop area stuff
+let dropArea = document.getElementById('drop-area')
+
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+})
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+;['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('highlight')
+}
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
+
+  handleFiles(files)
+}
+
+function handleFiles(files) {
+  ([...files]).forEach(uploadFile)
+}
+
+function uploadFile(file) {
+  let url = 'myWebsiteURL'
+  let formData = new FormData()
+
+  formData.append('file', file)
+
+  fetch(url, {
+    method: 'POST',
+    body: formData
+  })
+  .then(() => { /* Done. Inform the user */ })
+  .catch(() => { /* Error. Inform the user */ })
+}
+
+function uploadFile(file) {
+  var url = myWebsiteURL;
+  var xhr = new XMLHttpRequest()
+  var formData = new FormData()
+  xhr.open('POST', url, true)
+
+  xhr.addEventListener('readystatechange', function(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // Done. Inform the user
+    }
+    else if (xhr.readyState == 4 && xhr.status != 200) {
+      // Error. Inform the user
+    }
+  })
+
+  formData.append('file', file)
+  xhr.send(formData)
+}
